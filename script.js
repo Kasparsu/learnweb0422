0022ccfb535ffdae216978b4cc78bd8f2dd2b707
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-04-25 20:37:09 +0300

    Add types examples

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-// one line comment
-// still one line
-
-/** 
-    cool
-    multi 
-    line
-    comment
-    block
-*/
-
+let foo;
+foo=1;
+console.log(foo);
+let bar=2;
+foo=3;
+console.log(foo);
+console.log(typeof foo);
+foo='hello';
+console.log(foo);
+console.log(typeof foo);
+foo=1.4;
+console.log(foo);
+console.log(typeof foo);
+foo=true;
+console.log(foo);
+console.log(typeof foo);
+foo=false;
+console.log(foo);
+console.log(typeof foo);
+foo=null;
+console.log(foo);
+console.log(typeof foo);
+let baz;
+console.log(baz);
+console.log(typeof baz);
+baz=undefined;
+console.log(baz);
+console.log(typeof baz);
+foo=[1,2,3,4,5,'hello', true];
+console.log(foo);
+console.log(typeof foo);
+foo={name:'Kaspar', age:29};
+console.log(foo);
+console.log(typeof foo);
