df4077722c4f797fe07906874097dcba7880589e
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-04-27 21:27:56 +0300

    Add object example

--- script.js
+++ script.js
@@ -1,38 +1,9 @@
-let array = [1,2,3,4];
-array.push(5,6,3,2,1,4,3);
-array.pop();
-array.reverse();
-array.sort();
+let person = {
+    name: 'kaspar',
+    age: 12,
+    'last name': 'Suursalu' 
+};
+let key = 'name';
+console.log(person[key]);
 
-console.log(array[2]);
-let array2d = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-];
-
-console.log(array2d[1][2]);
-let array3d = [
-    [
-        [1,2,3],
-        [1,2,3],
-        [1,2,3]
-    ],
-    [
-        [1,2,3],
-        [1,2,3],
-        [1,2,3]
-    ],
-    [
-        [1,2,3],
-        [1,2,3],
-        [1,2,3]
-    ],
-];
-
-console.log(array3d[1][2][0]);
-let array4d = [[[[1]]]];
-console.log(array4d[0][0][0][0])
-
-let name = 'Kaspar';
-console.log(name[3]);
+console.log('hello ' + person.name + ' ' + person['last name']);
