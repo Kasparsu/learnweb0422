4b323af0a25b2abb3f457f33e4dedb5bebcceeb1
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-04-27 21:19:28 +0300

    Add array examples

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-let number = Math.floor(Math.random()*100)+1;
-let guess;
-while(number!=guess){
-    guess = parseInt(prompt('Arva number 1-100neni'));
-    if(number > guess){
-        alert('Suurem');
-    } else if(number < guess){
-        alert('Väiksem');
-    } else {
-        alert('Õige! Võitsid');
-    }
-}
+let array = [1,2,3,4];
+array.push(5,6,3,2,1,4,3);
+array.pop();
+array.reverse();
+array.sort();
+
+console.log(array[2]);
+let array2d = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+
+console.log(array2d[1][2]);
+let array3d = [
+    [
+        [1,2,3],
+        [1,2,3],
+        [1,2,3]
+    ],
+    [
+        [1,2,3],
+        [1,2,3],
+        [1,2,3]
+    ],
+    [
+        [1,2,3],
+        [1,2,3],
+        [1,2,3]
+    ],
+];
+
+console.log(array3d[1][2][0]);
+let array4d = [[[[1]]]];
+console.log(array4d[0][0][0][0])
+
+let name = 'Kaspar';
+console.log(name[3]);
