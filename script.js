b96f5093be67267c651dee7e1275b2648ed791d1
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-04-27 19:46:06 +0300

    Project euler problem 1

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-for(let i=0;i<10;i++){
-    console.log(i);
+let sum = 0;
+for(let i=0; i<1000; i++){
+    if(i%3==0 || i%5==0) {
+        sum+=i;
+    }
 }
-
-for(let i=10;i<20;i++){
-    console.log(i);
-}
-
-for(let i=10;i>0;i--){
-    console.log(i);
-}
-
-for(let i=1;i<2000;i*=2){
-    console.log(i);
-}
+console.log(sum);
