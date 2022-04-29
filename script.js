3380629cf0944041ecc7fa8fa3a4128d7e66636c
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-04-29 20:08:42 +0300

    Button event example

--- script.js
+++ script.js
@@ -1,28 +1,7 @@
-let nums = [1,2,3,4,5,6];
+console.log({doc: document});
+let button = document.getElementById('button');
 
-let nums4 = nums.map(a => a*4);
-
-let nums5 = nums.map(a => {
-    if(a%2==0){
-        return a*5;
-    }
-    return a;
+button.addEventListener('click', evt => {
+    console.log(evt);
+    document.getElementById('message').innerHTML = 'Hello';
 });
-
-let odd = nums.filter(function(a) {
-    return a%2==1;
-});
-let odd2 = nums.filter(a => a%2==1);
-
-let sum = nums.reduce((lastValue,currentValue) => lastValue+currentValue);
-let total = nums.reduce((lastValue,currentValue) => lastValue*currentValue);
-
-for(let i = 0; i<nums.length;i++){
-    console.log(nums[i]);
-}
-
-nums.forEach(a => {
-    console.log(a);
-});
-
-console.log(nums, nums4, nums5, odd, odd2, sum, total);
