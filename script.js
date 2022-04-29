582188dc9685895f505553bd0c0352aced95976b
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-04-29 18:09:46 +0300

    Add array and object to object example

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 let person = {
     name: 'kaspar',
     age: 12,
-    'last name': 'Suursalu' 
+    'last name': 'Suursalu',
+    books: [
+        'Harry Potter',
+        'Martian',
+        'Mees kes teadis ussisõnu'
+    ],
+    address: {
+        street: 'Rae',
+        city: 'Tallinn',
+        country: 'Estonia'
+    }
 };
 let key = 'name';
 console.log(person[key]);
 
 console.log('hello ' + person.name + ' ' + person['last name']);
+
+console.log(person.books[1]);
+console.log(person.address.country);
