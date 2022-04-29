dfbfb117d75921287041ca706ad896c08d6338bb
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-04-29 18:51:22 +0300

    Add function examples

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
-let person = {
-    name: 'kaspar',
-    age: 12,
-    'last name': 'Suursalu',
-    books: [
-        'Harry Potter',
-        'Martian',
-        'Mees kes teadis ussisõnu'
-    ],
-    address: {
-        street: 'Rae',
-        city: 'Tallinn',
-        country: 'Estonia'
+
+function add(a, b){
+    return a+b;
+}
+
+let divide = function(a,b) {
+    if(b!=0){
+        return a/b;
     }
-};
-let key = 'name';
-console.log(person[key]);
+    return NaN;
+}
 
-console.log('hello ' + person.name + ' ' + person['last name']);
+let sub = (a,b) => a-b;
 
-console.log(person.books[1]);
-console.log(person.address.country);
+let person = {
+    hello() {
+        console.log('hello');
+    } 
+}
+
+
+let sum = add(3,5);
+let sum2 = add(2,6);
+console.log(sum, sum2);
+let value = divide(4,0);
+console.log(value);
+value = sub(4,3);
+console.log(value);
+person.hello();
+
+function recursive(i){
+    console.log(i);
+    if(i<10){
+        recursive(i+1);
+    }
+}
+recursive(0);
