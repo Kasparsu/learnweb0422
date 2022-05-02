a0f75a133c58f00e6042a5637a8585829da4f496
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-05-02 19:03:50 +0300

    Add removing items

--- script.js
+++ script.js
@@ -2,9 +2,21 @@ let addItem = document.querySelector('#addItem');
 let newItem = document.querySelector('#newItem');
 let shoppingList = document.querySelector('#shoppingList');
 
+let addRemoveEvents = () => {
+    let checkboxes = document.querySelectorAll('#shoppingList>li>input');
+    checkboxes.forEach(check => {
+        check.addEventListener('click', event => {
+            event.target.parentElement.remove();
+        });
+    });
+}
+
+addRemoveEvents();
+
 let addingItem = ()=> {
     if(newItem.value.trim() != ''){
-        shoppingList.innerHTML += '<li>' + newItem.value + '</li>';
+        shoppingList.innerHTML += '<li>' + newItem.value + ' <input type="checkbox"></li>';
+        addRemoveEvents();
     }
     newItem.value = '';
 }
@@ -12,8 +24,8 @@ let addingItem = ()=> {
 
 addItem.addEventListener('click', addingItem);
 newItem.addEventListener('keydown', event => {
-    console.log(event);
     if(event.code == 'Enter'){
         addingItem();
     }
 });
+
